bff299864d750d8ad13479a42e64afd3e68d7274
BAJI BABU SHAIK <BAJI BABU SHAIK@LAPTOP-MHTJ6FP0>
2019-04-06 17:03:49 +0530

    initial code write

---
+++ src/main/java/git/learn/Sample.java
@@ -0,0 +1,12 @@
+package git.learn;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Initiall push to Github");
+
+	}
+
+}
